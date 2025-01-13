2716ac7c41e451d89dc168c4f4cf659bb40bf4c6
bcenand <berkancenan@hotmail.com>
2025-01-13 14:55:09 +0300

    Character Creation Update.

--- Assets/ThirdParty/Game Kit Controller/Scripts/Others/pathInfoValues.cs
+++ Assets/ThirdParty/Game Kit Controller/Scripts/Others/pathInfoValues.cs
@@ -6,127 +6,127 @@ public class pathInfoValues : MonoBehaviour
 {
     public static string getMainInventoryManagerPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Inventory/Main Inventory Manager (Inventory, Bank and Vendor).prefab";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Inventory/Main Inventory Manager (Inventory, Bank and Vendor).prefab";
     }
 
     public static string getMeleeWeaponObjectPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Melee Combat System/Melee Weapons/";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Melee Combat System/Melee Weapons/";
     }
 
     public static string getPhysicalObjectsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Objects To Grab Physically/Templates/Regular Objects Templates/";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Objects To Grab Physically/Templates/Regular Objects Templates/";
     }
 
     public static string getInventoryMeshShieldPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Inventory/Mesh/Melee Shields";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Inventory/Mesh/Melee Shields";
     }
 
     public static string getMeleeShieldObjectPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Melee Combat System/Melee Shields";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Melee Combat System/Melee Shields";
     }
 
     public static string getMeleeShieldObjectPrefabPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Melee Combat System/Melee Shield Object Template.prefab";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Melee Combat System/Melee Shield Object Template.prefab";
     }
 
     public static string getInventoryInfoCapturesPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Inventory/Captures";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Inventory/Captures";
     }
 
     public static string getTransparentInventoryCaptureToolScenePath ()
     {
-        return "Assets/Game Kit Controller/Scenes/Inventory Icon Creator/Create Inventory Transparent Icon.unity";
+        return "Assets/ThirdParty/Game Kit Controller/Scenes/Inventory Icon Creator/Create Inventory Transparent Icon.unity";
     }
 
     public static string getSliceObjectsPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Others/Slice System";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Others/Slice System";
     }
 
     public static string getUsableWeaponsPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Weapons/Usable Weapons";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Weapons/Usable Weapons";
     }
 
     public static string getPrefabsManagerPrefabPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Prefabs Manager/Prefabs Manager.prefab";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Prefabs Manager/Prefabs Manager.prefab";
     }
 
     public static string getWeaponPickupsPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Weapons/Weapons Pickups";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Weapons/Weapons Pickups";
     }
 
     public static string getWeaponMeshInventoryPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Inventory/Mesh/Weapons";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Inventory/Mesh/Weapons";
     }
 
     public static string getWeaponAmmoMeshInventoryPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Inventory/Mesh/Ammo";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Inventory/Mesh/Ammo";
     }
 
     public static string getGenericCharacterPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Generic Model System/Generic Model Controllers Prefabs/";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Generic Model System/Generic Model Controllers Prefabs/";
     }
 
     public static string getVehiclePrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Vehicles/Vehicles/";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Vehicles/Vehicles/";
     }
 
     public static string getGenericCharacterToRidePrefabPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Vehicles/Vehicles/Animals/Template/Generic Model Vehicle Template.prefab";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Vehicles/Vehicles/Animals/Template/Generic Model Vehicle Template.prefab";
     }
 
     public static string getRegularActionSystemPrefabPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Action System/Regular Action System Template.prefab";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Action System/Regular Action System Template.prefab";
     }
 
     public static string getMeleeActionSystemPrefabPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Action System/Melee Attack Action System Template.prefab";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Action System/Melee Attack Action System Template.prefab";
     }
 
     public static string getArmorClothPrefabPath ()
     {
-        return "Assets/Game Kit Controller/Scriptable Objects/Character Customization/Armor Cloth Templates/";
+        return "Assets/ThirdParty/Game Kit Controller/Scriptable Objects/Character Customization/Armor Cloth Templates/";
     }
 
     public static string getFullSetsCharacterCreationTemplatesPrefabPath ()
     {
-        return "Assets/Game Kit Controller/Scriptable Objects/Character Customization/Character Creation Templates/Full Sets/";
+        return "Assets/ThirdParty/Game Kit Controller/Scriptable Objects/Character Customization/Character Creation Templates/Full Sets/";
     }
 
     public static string getArmorClothMeshInventoryPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/Inventory/Mesh/Armor Cloth Pieces";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/Inventory/Mesh/Armor Cloth Pieces";
     }
 
     public static string getCraftingBlueprintPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Scriptable Objects/Crafting System";
+        return "Assets/ThirdParty/Game Kit Controller/Scriptable Objects/Crafting System";
     }
 
     public static string getCraftingBlueprintDataPrefabsPath ()
     {
-        return "Assets/Game Kit Controller/Scriptable Objects/Crafting System/Main Blueprint List";
+        return "Assets/ThirdParty/Game Kit Controller/Scriptable Objects/Crafting System/Main Blueprint List";
     }
 
     public static string getMainPrefabsFolderPath ()
     {
-        return "Assets/Game Kit Controller/Prefabs/";
+        return "Assets/ThirdParty/Game Kit Controller/Prefabs/";
     }
 
     public static string getMainPlayerPrefabSubFolderPath ()
